e3ffb759d3a7c7254638b3e32236d99242989090
Norbert <n.sendetzky@metaways.de>
2013-10-03 18:38:42 +0200

    Moves Zend related MWLib adapters to new Arcavias Zend extension

---
+++ manifest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Metaways Infosystems GmbH, 2013
+ */
+
+return array(
+	'name' => 'zend',
+	'depends' => array(
+		'arcavias-core',
+	),
+	'include' => array(
+		'lib/custom/src',
+	),
+	'config' => array(
+	),
+	'setup' => array(
+	),
+);
